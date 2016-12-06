373fd409ddd7201ae70bef8c83bd0c9f03d4e242
ocramz <zocca.marco@gmail.com>
2016-12-06 18:27:34 +0100

    first

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,17 @@
--- | A library to do stuff.
-module Lib
-    (
-      ourAdd
-    ) where
+module Lib where
 
--- | Add two 'Int' values.
-ourAdd :: Int  -- ^ left
-       -> Int  -- ^ right
-       -> Int  -- ^ sum
-ourAdd x y = x + y
+import Control.Monad.Par
+
+-- import Control.Monad.Par.Scheds.Trace
+
+asdf :: (NFData a, Num a, Enum a) => a -> a -> a
+asdf m n = runPar $ do
+      i <- new                          -- 1
+      j <- new                          -- 2
+      fork (put i (sumv n))              -- 3
+      fork (put j (sumv m))              -- 4
+      a <- get i                        -- 5
+      b <- get j                        -- 6
+      return (a + b) where
+        sumv nn = sum [0 .. nn]
+
